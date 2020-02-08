14ffc615f2bd2c5f1d16aad47718746012ad62b8
Carlos R. Guerra <cguerramain@gmail.com>
2020-02-07 20:01:48 -0500

    Updated predict.py cli

--- deeph3/predict.py
+++ deeph3/predict.py
@@ -1,23 +1,46 @@
 import torch
 import torch.nn as nn
+import argparse
+import pickle
+import os
 from deeph3 import H3ResNet
 from deeph3.util import get_probs_from_model, bin_matrix
 from os.path import isfile
 
 
-def predict(model, fasta_file, chain_delimiter=True, binning_method='max'):
+def predict(model, fasta_file, chain_delimiter=True, binning_method='max',
+            return_raw_probabilities=False):
     """
-    Predicts the binned version of a distance matrix
+    Predicts the binned version of a fasta file's inter-residue distance and
+    angle matrices. Alternatively, the raw probability distribution outputs
+    can be returned.
     """
     probs = get_probs_from_model(model, fasta_file, chain_delimiter=chain_delimiter)
-    dist, omega, theta, phi = bin_matrix(probs, are_logits=False,
-                                         method=binning_method)
+    if return_raw_probabilities:
+        dist, omega, theta, phi = probs
+    else:
+        dist, omega, theta, phi = bin_matrix(probs, are_logits=False,
+                                             method=binning_method)
     return dict(distance_matrix=dist, omega_matrix=omega,
                 theta_matrix=theta, phi_matrix=phi)
 
 
 def load_model(file_name, num_blocks1D=3, num_blocks2D=21):
-    """Loads a model from a correctly named file"""
+    """Loads a model from a pickle file
+
+    :param file_name:
+        A pickle file containing a dictionary with the following keys:
+            state_dict: The state dict of the H3ResNet model
+            num_blocks1D: The number of one dimensional ResNet blocks
+            num_blocks2D: The number of two dimensional ResNet blocks
+            dilation (optional): The dilation cycle of the model
+    :param num_blocks1D:
+        If num_blocks1D is not in the pickle file, then this number is used for
+        the amount of one dimensional residual blocks.
+    :param num_blocks2D:
+        If num_blocks2D is not in the pickle file, then this number is used for
+        the amount of two dimensional residual blocks.
+    """
     if not isfile(file_name):
         raise FileNotFoundError(f'No file at {file_name}')
     checkpoint_dict = torch.load(file_name, map_location='cpu')
@@ -46,11 +69,7 @@ def load_model(file_name, num_blocks1D=3, num_blocks2D=21):
     return model
 
 
-if __name__ == '__main__':
-    import argparse
-    import pickle
-    import os
-
+def cli():
     predict_py_path = os.path.dirname(os.path.realpath(__file__))
     default_model_path = os.path.join(predict_py_path, 'models/fully_trained_model.p')
     default_fasta_path = os.path.join(predict_py_path, 'data/antibody_dataset/fastas_testrun/1a0q_trunc.fasta')
@@ -75,10 +94,20 @@ if __name__ == '__main__':
     parser.add_argument('--fasta_file', type=str,
                         default=default_fasta_path,
                         help='The fasta file used for prediction.')
+    parser.add_argument('--output_raw_probabilities', type=bool,
+                        default=False,
+                        help=('Flag to output the raw probability distributions '
+                              'rather than the binned versions'))
     parser.add_argument('--out_file', type=str,
                         default='model_out.p',
                         help='The pickle file to save the model output to.')
     args = parser.parse_args()
     model = load_model(args.model_file)
-    pickle.dump(predict(model, args.fasta_file), open(args.out_file, 'wb'))
+    predictions = predict(model, args.fasta_file,
+                          return_raw_probabilities=args.output_raw_probabilities)
+    pickle.dump(predictions, open(args.out_file, 'wb'))
+
+
+if __name__ == '__main__':
+    cli()
 
