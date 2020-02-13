2070e77f1b6aa41d44eaf947cabb55382a5c2cfb
Jeffrey Gray <jgray@jhu.edu>
2020-02-13 08:29:02 -0500

    remove stray character causing syntax fail

--- deeph3/predict.py
+++ deeph3/predict.py
@@ -42,7 +42,7 @@ def load_model(file_name, num_blocks1D=3, num_blocks2D=21):
         the amount of two dimensional residual blocks.
     """
     if not isfile(file_name):
-        raise FileNotFoundError(f'No file at {file_name}')
+        raise FileNotFoundError('No file at {file_name}')
     checkpoint_dict = torch.load(file_name, map_location='cpu')
     model_state = checkpoint_dict['model_state_dict']
 
